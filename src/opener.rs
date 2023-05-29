6f51aca6b8b1dc061204436c7b2ec0792b91569d
Christian Hammermeister <christian.hammermeister@iml.fraunhofer.de>
2023-05-29 18:16:38 +0200

    opener uses mime-types

--- src/opener.rs
+++ src/opener.rs
@@ -26,34 +26,60 @@ impl OpenEngine {
             .queue(Clear(ClearType::All))?
             .queue(cursor::MoveTo(0, 0))?;
         stdout.flush()?;
-        // Image
-        // If the selected item is a file,
-        // we need to open it
-        if let Some(ext) = absolute.extension().and_then(|ext| ext.to_str()) {
-            match ext {
-                "png" | "bmp" | "jpg" | "jpeg" | "svg" => {
-                    Command::new("sxiv")
-                        .stderr(Stdio::null())
-                        .stdin(Stdio::null())
-                        .stdout(Stdio::null())
-                        .arg(absolute.clone())
-                        .spawn()?;
-                }
-                "wav" | "aiff" | "au" | "flac" | "m4a" | "mp3" | "opus" => {
-                    Command::new("mpv").arg(absolute).spawn()?.wait()?;
-                }
+        // Check mime-type
+
+        let mime_type = mime_guess::from_path(&absolute).first_or_text_plain();
+        match mime_type.type_().as_str() {
+            "image" => {
+                Command::new("sxiv")
+                    .stderr(Stdio::null())
+                    .stdin(Stdio::null())
+                    .stdout(Stdio::null())
+                    .arg(absolute.clone())
+                    .spawn()?;
+            }
+            "audio" | "video" => {
+                Command::new("mpv").arg(absolute).spawn()?.wait()?;
+            }
+            "application" => match mime_type.subtype().as_str() {
                 "pdf" => {
                     Command::new("zathura").arg(absolute).spawn()?;
                 }
                 _ => {
-                    // Everything else with vim
                     Command::new("nvim").arg(absolute).spawn()?.wait()?;
                 }
+            },
+            _ => {
+                // Everything else with vim
+                Command::new("nvim").arg(absolute).spawn()?.wait()?;
             }
-        } else {
-            // Try to open things without extensions with vim
-            Command::new("nvim").arg(absolute).spawn()?.wait()?;
         }
+
+        // if let Some(ext) = absolute.extension().and_then(|ext| ext.to_str()) {
+        //     match ext {
+        //         "png" | "bmp" | "jpg" | "jpeg" | "svg" => {
+        //             Command::new("sxiv")
+        //                 .stderr(Stdio::null())
+        //                 .stdin(Stdio::null())
+        //                 .stdout(Stdio::null())
+        //                 .arg(absolute.clone())
+        //                 .spawn()?;
+        //         }
+        //         "wav" | "aiff" | "au" | "flac" | "m4a" | "mp3" | "opus" => {
+        //             Command::new("mpv").arg(absolute).spawn()?.wait()?;
+        //         }
+        //         "pdf" => {
+        //             Command::new("zathura").arg(absolute).spawn()?;
+        //         }
+        //         _ => {
+        //             // Everything else with vim
+        //             Command::new("nvim").arg(absolute).spawn()?.wait()?;
+        //         }
+        //     }
+        // } else {
+        //     // Try to open things without extensions with vim
+        //     Command::new("nvim").arg(absolute).spawn()?.wait()?;
+        // }
         terminal::enable_raw_mode()?;
         Ok(())
     }
