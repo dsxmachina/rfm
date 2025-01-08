7dbd54649f8d2a304f006fe2a0bfcf2264febeb9
Christian Hammermeister <christian.hammermeister@tu-dortmund.de>
2025-01-08 16:42:36 +0100

    bump version and add changelog

---
+++ changelog.md
@@ -0,0 +1,6 @@
+# 0.3.0
+
+- fix incorrect display width of exotic unicode characters
+- add general config-file `config.toml`
+- move color config to general config
+- add input parameter for startup path
--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
         # Define the rfm package
         rfm = pkgs.rustPlatform.buildRustPackage {
           pname = "rfm";
-          version = "0.2.11";
+          version = "0.3.0";
           src = ./.;
           cargoLock = { 
             lockFile = ./Cargo.lock; 
